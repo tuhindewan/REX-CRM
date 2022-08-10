692e58056671712d66c86a5df0182855959a6588
ayoncoderex <ayon@coderex.co>
2022-08-10 12:15:43 +0600

    Change default msg in base controller

--- includes/core/controllers/class-mrm-base-controller.php
+++ includes/core/controllers/class-mrm-base-controller.php
@@ -7,22 +7,22 @@ use WP_REST_Request;
 use WP_REST_Response;
 
 abstract class MRM_Base_Controller {
-  public function get_success_response(string $msg, int $status = 200 ) {
+  public function get_success_response(string $msg = "Successfully performed operation", int $status = 200 ) {
     return new WP_REST_Response(
       array(
         'success' => true,
-        'msg' => isset($msg) ? $msg : "Successfully performed operation"
+        'msg' => $msg 
       ),
       $status
     );
   }
-  public function get_error_response(string $msg, int $code  ) {
+  public function get_error_response(string $msg = "Failed to perform operation", int $code  ) {
     return new WP_Error(
       $code,
       $msg,
       array(
         'success' => false,
-        'msg' => isset($msg) ? $msg : "Failed to perform operation"
+        'msg' => $msg 
       ),
     );
   }
