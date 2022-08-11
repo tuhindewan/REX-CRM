befd019ab862ebb256e13d5b8aa0844466ba000e
sadi1991 <sadi@coderex.co>
2022-08-11 11:59:46 +0600

    test updating version

--- mrm.php
+++ mrm.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MRM_VERSION', '1.0.0' );
+define( 'MRM_VERSION', '1.0.1' );
 
 
 add_action( 'admin_menu', 'crm_init_menu' );
