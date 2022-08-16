076d02fadd9cfe44b27d6a66412856976d97989e
ayoncoderex <ayon@coderex.co>
2022-08-16 12:26:13 +0600

    Add auto refresh frontend

--- admin/asset/dist/main.asset.php
+++ admin/asset/dist/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '7001287759be27c9b4fb');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-react-refresh-runtime'), 'version' => '7bf7b08b226f09dc742b');
--- admin/src/components/Dashboard.js
+++ admin/src/components/Dashboard.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div className='dashboard'>
-            <div className="card">
-                <h3>Dashboard</h3>
-                <p>
-                    Edit Dashboard component at <code>src/components/Dashboard.jsx</code>
-                </p>
-            </div>
-        </div>
-     );
-}
+  return (
+    <div className="dashboard">
+      <div className="card">
+        <h3>Dashboard</h3>
+        <p>
+            lskdjflsdkfjsld
+          Hello from the other side yay Dashboard co sfasdfas dfmponent at{" "}
+          <code>src/components/Dashboard.jsx</code>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
