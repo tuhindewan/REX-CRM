e6c37431932967ccdb84531e9eb15360fa79fcef
sazzad_sowmik <sazzadul@coderex.co>
2022-09-16 12:52:40 +0600

    resolved isset issue

--- app/Admin/API/Controllers/ContactController.php
+++ app/Admin/API/Controllers/ContactController.php
@@ -127,12 +127,12 @@ class ContactController extends BaseController {
         $contact    = ContactModel::get( $params['contact_id'] );
         
         // Get and merge tags and lists
-        if( isset($contact) ) {
+        if( $contact ) {
             $contact    = TagController::get_tags_to_contact( $contact );
             $contact    = ListController::get_lists_to_contact( $contact );
         }
         
-        if(isset($contact)) {
+        if($contact && isset($contact['email'])) {
             $avatar_url   = 'https://www.gravatar.com/avatar/0?s=80&d=retro';
 
             $avatar_url = 'https://www.gravatar.com/avatar/' . md5( $contact['email']) . '?s=80&&d=retro';
