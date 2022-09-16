8e6bbfaf153d1fd563888174b0aed9ffbc5f5fa3
ayoncoderex <ayon@coderex.co>
2022-09-16 15:58:26 +0600

    Add custom select

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,13 @@ import CustomSelect from "./components/CustomSelect";
 import Header from "./components/Navbar/";
 import routes from "./routes/index.js";
 
-import { useState } from "react";
-
 const App = () => {
-  const [active, setActive] = useState(false);
-  const [selected, setSelected] = useState([]);
   return (
     <div className="mintmrm">
       <ReactFlowProvider>
         <HashRouter>
           <>
             <Header />
-            <CustomSelect
-              active={active}
-              setActive={setActive}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
             <Routes>
               {routes.map((route, index) => (
                 <Route
--- src/components/CustomSelect/index.jsx
+++ src/components/CustomSelect/index.jsx
@@ -1,23 +1,28 @@
 import { useState, useEffect, useRef, isValidElement } from "react";
 import Search from "../Icons/Search";
+import LoadingIndicator from "../LoadingIndicator";
+import CrossIcon from "../Icons/CrossIcon";
 
 export default function CustomSelect(props) {
   const {
     active,
     setActive,
-    showSearchBar,
     selected,
     setSelected,
-    fixedOptions = false,
     options = null,
     endpoint = "/lists",
     page = 1,
     perPage = 20,
     placeholder = "Lists",
+    name = "list",
     listTitle = "CHOOSE LIST",
+    listTitleOnNotFound = "No Data Found",
     searchPlaceHolder = "Search...",
-    multiple = false,
-    allowNewCreate = false,
+    allowMultiple = true,
+    showSearchBar = true,
+    showListTitle = true,
+    showSelectedInside = true,
+    allowNewCreate = true,
   } = props;
   const buttonRef = useRef(null);
   const inputRef = useRef(null);
@@ -29,6 +34,9 @@ export default function CustomSelect(props) {
 
   // search query, search query only updates when there are more than 3 characters typed
   const [query, setQuery] = useState("");
+
+  // loading or not
+  const [loading, setLoading] = useState(false);
 
   function toggleActive(event) {
     event.stopPropagation();
@@ -46,11 +54,11 @@ export default function CustomSelect(props) {
 
   // handler for one single item click
   const handleSelectOne = (e) => {
-    console.log(e.target);
+    e.stopPropagation();
     const index = selected.findIndex((item) => item.id == e.target.id);
 
     // already in selected list so remove it from the array
-    if (multiple) {
+    if (allowMultiple) {
       if (index >= 0) {
         setSelected(selected.filter((item) => item.id != e.target.id));
       } else {
@@ -61,8 +69,6 @@ export default function CustomSelect(props) {
       if (index >= 0) setSelected([]);
       else setSelected([{ id: e.target.id, title: e.target.value }]);
     }
-
-    console.log(selected);
   };
 
   const checkIfSelected = (id) => {
@@ -70,21 +76,70 @@ export default function CustomSelect(props) {
     return checked;
   };
 
+  const deleteSelected = (e, id) => {
+    console.log("hello");
+    console.log(id);
+    const index = selected.findIndex((item) => item.id == id);
+
+    // already in selected list so remove it from the array
+    if (allowMultiple) {
+      if (index >= 0) {
+        setSelected(selected.filter((item) => item.id != id));
+      }
+    } else {
+      if (index >= 0) setSelected([]);
+    }
+  };
+
   // at first page load get all the available lists
   useEffect(() => {
-    console.log("useeffect");
-
     async function getItems() {
+      setLoading(true);
       const res = await fetch(
         `${window.MRM_Vars.api_base_url}mrm/v1${endpoint}?&page=${page}&per-page=${perPage}${query}`
       );
       const resJson = await res.json();
       if (resJson.code == 200) {
         setItems(resJson.data.data);
+        setLoading(false);
+      } else {
+        console.log(resJson);
       }
     }
-    getItems();
+    if (!options) getItems();
   }, [query]);
+
+  // Handle list create or update form submission
+  const addNewItem = async () => {
+    let res = null;
+    let body = {
+      title: search,
+      slug: search.toLowerCase().replace(/[\W_]+/g, "-"),
+    };
+    try {
+      // create contact
+      setLoading(true);
+      res = await fetch(`${window.MRM_Vars.api_base_url}mrm/v1${endpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const resJson = await res.json();
+      if (resJson.code == 201) {
+        setSearch("");
+        setQuery("");
+        setSelected([...selected, { id: resJson.data, title: body.title }]);
+      } else {
+        window.alert(resJson.message);
+      }
+    } catch (e) {
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -98,7 +153,17 @@ export default function CustomSelect(props) {
         >
           {placeholder}
         </button>
-
+        {/* <div className="mrm-selected-items-container">
+          {showSelectedInside &&
+            selected.map((item) => {
+              return (
+                <span key={item.id} className="mrm-custom-selected-items">
+                  {item.title}
+                  <CrossIcon onClick={(e) => deleteSelected(e, item.id)} />
+                </span>
+              );
+            })}
+        </div> */}
         <ul
           className={
             active
@@ -106,24 +171,54 @@ export default function CustomSelect(props) {
               : "mintmrm-dropdown mrm-custom-select-dropdown"
           }
         >
-          <li className="searchbar" key="hello">
-            <span class="pos-relative">
-              <Search />
-              <input
-                type="search"
-                name="column-search"
-                placeholder="Search..."
-                value={search}
-                onChange={handleSearch}
-              />
-            </span>
-          </li>
-          <li className="list-title" key="world">
-            {listTitle}
-          </li>
+          {showSearchBar && (
+            <li className="searchbar" key="hello">
+              <span class="pos-relative">
+                <Search />
+                <input
+                  type="search"
+                  name="column-search"
+                  placeholder={searchPlaceHolder}
+                  value={search}
+                  onChange={handleSearch}
+                />
+              </span>
+            </li>
+          )}
+          {showListTitle && (
+            <li className="list-title" key="world">
+              {!loading
+                ? items?.length && !options == 0
+                  ? listTitleOnNotFound
+                  : listTitle
+                : null}
+            </li>
+          )}
 
           {/* Render all elements */}
+          {options &&
+            options.map((item, index) => {
+              return (
+                <li key={index} className="single-column">
+                  <div class="mintmrm-checkbox">
+                    <input
+                      type="checkbox"
+                      name={item.id}
+                      id={item.id}
+                      value={item.title}
+                      onChange={handleSelectOne}
+                      checked={checkIfSelected(item.id)}
+                    />
+                    <label for={item.id} className="mrm-custom-select-label">
+                      {item.title}
+                    </label>
+                  </div>
+                </li>
+              );
+            })}
           {items?.length > 0 &&
+            !options &&
+            !loading &&
             items.map((item, index) => {
               return (
                 <li key={index} className="single-column">
@@ -136,19 +231,24 @@ export default function CustomSelect(props) {
                       onChange={handleSelectOne}
                       checked={checkIfSelected(item.id)}
                     />
-                    <label for={item.id}>{item.title}</label>
+                    <label for={item.id} className="mrm-custom-select-label">
+                      {item.title}
+                    </label>
                   </div>
                 </li>
               );
             })}
-          {items?.length == 0 && (
+          {items?.length == 0 && allowNewCreate && !loading && !options && (
             <>
-              <li className="not-found">No data found</li>
-              <button className="mrm-custom-select-add-btn">
-                + Add new list "{search}"
+              <button
+                className="mrm-custom-select-add-btn"
+                onClick={addNewItem}
+              >
+                {`+ Add new ${name} "${search}"`}
               </button>
             </>
           )}
+          {loading && <LoadingIndicator type="table" />}
         </ul>
       </div>
     </>
