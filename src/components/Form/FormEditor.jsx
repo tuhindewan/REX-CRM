ddc7fea87d6184185fec8dc0a301ec856603eb88
Md hr shahin <shahin@coderex.co>
2022-10-06 17:43:09 +0600

    Save button

--- src/components/Form/FormEditor.jsx
+++ src/components/Form/FormEditor.jsx
@@ -7,13 +7,17 @@ const FormEditor = () => {
   const [selectedLists, setSelectedLists] = useState([]);
   const [selectedTags, setSelectedTags] = useState([]);
 
+  const  saveForm = () =>{
+    const content = document.getElementById('mrm-block-editor')
+
+  }
 
   return (
     <>
         <div className="add-contact-form">
           <div className="contact-form-body">
             <InputItem label="Title" name="form_title" />
-            <button type="submit" className="contact-save mintmrm-btn ">
+            <button type="submit" className="contact-save mintmrm-btn " onClick={saveForm}>
               Save
             </button>
             <div id="mrm-block-editor" className="getdave-sbe-block-editor block-editor"></div>
