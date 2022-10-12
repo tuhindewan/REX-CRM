f8a20a526661d34405af03be569e3410da96b017
sazzad_sowmik <sazzadul@coderex.co>
2022-10-12 12:00:03 +0600

    search function with 1 char working

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -263,7 +263,24 @@ export default function FormIndex(props) {
 
                   <span className="search-section">
                     <Search />
-                    <input type="text" placeholder="Search..." />
+                    <input
+                      type="text"
+                      placeholder="Search..."
+                      value={search}
+                      onChange={(e) => {
+                        let value = e.target.value;
+                        setSearch(value);
+                        // only set query when there are more than 1 characters
+                        if (value.length >= 1) {
+                          setQuery(`&search=${value}`);
+                          // on every new search term set the page explicitly to 1 so that results can
+                          // appear
+                          setPage(1);
+                        } else {
+                          setQuery("");
+                        }
+                      }}
+                    />
                   </span>
 
                   <div className="bulk-action">
