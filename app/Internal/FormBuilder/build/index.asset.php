539586cc8a465eb2f673bf3ead533b0af15256aa
abbelal <belal@coderex.co>
2022-10-18 11:58:15 +0600

    mrm form button block settings design done

--- app/Internal/FormBuilder/build/index.asset.php
+++ app/Internal/FormBuilder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-format-library', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-media-utils'), 'version' => 'f7b036ddc9834cd9e55e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-format-library', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-media-utils'), 'version' => 'd1897a08a35dbba96524');
