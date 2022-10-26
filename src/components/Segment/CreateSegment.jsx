479f55eaece8249a9443f907f9bb835b6bbc1545
abbelal <belal@coderex.co>
2022-10-26 16:37:08 +0600

    segment list static markup design done

--- src/components/Segment/CreateSegment.jsx
+++ src/components/Segment/CreateSegment.jsx
@@ -1,12 +1,61 @@
 import React from 'react'
 import { useState } from "react";
+import SingleCondition from './SingleCondition'
+
 import DoubleAngleLeftIcon from '../Icons/DoubleAngleLeftIcon'
 import DoubleAngleRightIcon from '../Icons/DoubleAngleRightIcon'
 import DeleteIcon from '../Icons/Delete'
 
 const CreateSegment = () => {
-    const [conditionDropdown, setConditionDropdown] = useState(false);
+    const [matchDropdown, setMatchDropdown] = useState(false);
 
+    const showMatchDropdown = () => {
+        setMatchDropdown(!matchDropdown);
+    };
+
+    let segmentConditions = [
+        { 
+            'field_type': {
+                'their-email-addresses': 'Their email addresses',
+                'their-last-names': 'Their last names',
+                'their-first-names': 'Their first names',
+                'their-mobile-numbers': 'Their mobile phone numbers',
+            }, 
+            'field_condition': {
+                'are-exactly': 'Are exactly',
+                'are-not': 'Are Not',
+            },
+            'field_action': {
+                'action-1': 'Action 1',
+                'action-2': 'Action 2',
+                'action-3': 'Action 3',
+                'action-4': 'Action 4',
+            },
+            'field_action_input': 'input-text',
+        },
+        { 
+            'field_type': {
+                'their-email-addresses': 'Their email addresses',
+                'their-last-names': 'Their last names',
+                'their-first-names': 'Their first names',
+                'their-mobile-numbers': 'Their mobile phone numbers',
+            }, 
+            'field_condition': {
+                'are-exactly': 'Are exactly',
+                'are-not': 'Are Not',
+            },
+            'field_action': {
+                'action-1': 'Action 1',
+                'action-2': 'Action 2',
+                'action-3': 'Action 3',
+                'action-4': 'Action 4',
+            },
+            'field_action_input': 'input-text',
+        },
+        
+        
+    ];
+      
     return (
         <>
             <div className="add-segment-page">
@@ -30,114 +79,27 @@ const CreateSegment = () => {
                         </div>
                     </div>
 
-                    <div className="condition-matching">of the following conditions:</div>
+                    <div className="condition-matching">
+                        <b>Match</b> 
+                        <div className={ matchDropdown ? "mrm-custom-select-container show-dropdown" : "mrm-custom-select-container" } >
+                            <button className="mrm-custom-select-btn" type="button" onClick={() => showMatchDropdown()} >All</button>
+                            <ul className="mintmrm-dropdown mrm-custom-select-dropdown">
+                                <li value="all">All</li>
+                                <li value="any">Any</li>
+                            </ul>
+                        </div>
+                        of the following conditions:
+                    </div>
+
                     <div className="segment-condition-wrapper">
-
-                        <div className="single-condition">
-                            <ul className="single-condition-inner">
-                                <li className="single-condition-filed field-type">
-                                    <select name="" id="">
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                    </select>
-                                </li>
-
-                                <li className="single-condition-filed field-condition">
-                                    <select name="" id="">
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                    </select>
-                                </li>
-
-                                <li className="single-condition-filed field-action">
-                                    <select name="" id="">
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                    </select>
-                                </li>
-
-                                <li className="single-condition-filed delete-condition">
-                                    <button type="button">
-                                        <DeleteIcon />
-                                    </button>
-                                </li>
-                            </ul>
-
-                            <div className="condition-repeater">
-                                <span className={ conditionDropdown ? "add-condition show" : "add-condition" } title="Add Condition">
-                                    <svg width="22" height="22" fill="none" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><g fill="#573BFF" clip-path="url(#clip0_616_1195)"><path d="M13 0C5.831 0 0 5.831 0 13s5.831 13 13 13 13-5.832 13-13S20.169 0 13 0zm0 23.986C6.943 23.986 2.014 19.058 2.014 13 2.014 6.942 6.944 2.014 13 2.014c6.057 0 10.986 4.928 10.986 10.986 0 6.058-4.928 10.986-10.986 10.986z"/><path d="M18.035 11.902h-4.028V7.875a1.007 1.007 0 10-2.014 0v4.027H7.965a1.007 1.007 0 100 2.014h4.028v4.028a1.007 1.007 0 102.014 0v-4.027h4.028a1.007 1.007 0 100-2.014z"/></g><defs><clipPath id="clip0_616_1195"><path fill="#fff" d="M0 0h26v26H0z"/></clipPath></defs></svg>
-
-                                    <ul className="conditions mintmrm-dropdown">
-                                        <li>and</li>
-                                        <li>or</li>
-                                    </ul>
-                                </span>
-                            </div>
-
-                            <div className="condition-symbol">
-                                <span className="symbol-and">&</span>
-                                <span className="symbol-or">or</span>
-                            </div>
-                        </div>
-
-                        <div className="single-condition">
-                            <ul className="single-condition-inner">
-                                <li className="single-condition-filed field-type">
-                                    <select name="" id="">
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                    </select>
-                                </li>
-
-                                <li className="single-condition-filed field-condition">
-                                    <select name="" id="">
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                    </select>
-                                </li>
-
-                                <li className="single-condition-filed field-action">
-                                    <select name="" id="">
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                        <option value="">field type</option>
-                                    </select>
-                                </li>
-
-                                <li className="single-condition-filed delete-condition">
-                                    <button type="button">
-                                        <DeleteIcon />
-                                    </button>
-                                </li>
-                            </ul>
-
-                            <div className="condition-repeater">
-                                <span className="add-condition" title="Add Condition">
-                                    <svg width="22" height="22" fill="none" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><g fill="#573BFF" clip-path="url(#clip0_616_1195)"><path d="M13 0C5.831 0 0 5.831 0 13s5.831 13 13 13 13-5.832 13-13S20.169 0 13 0zm0 23.986C6.943 23.986 2.014 19.058 2.014 13 2.014 6.942 6.944 2.014 13 2.014c6.057 0 10.986 4.928 10.986 10.986 0 6.058-4.928 10.986-10.986 10.986z"/><path d="M18.035 11.902h-4.028V7.875a1.007 1.007 0 10-2.014 0v4.027H7.965a1.007 1.007 0 100 2.014h4.028v4.028a1.007 1.007 0 102.014 0v-4.027h4.028a1.007 1.007 0 100-2.014z"/></g><defs><clipPath id="clip0_616_1195"><path fill="#fff" d="M0 0h26v26H0z"/></clipPath></defs></svg>
-
-                                    <ul className="conditions mintmrm-dropdown">
-                                        <li>and</li>
-                                        <li>or</li>
-                                    </ul>
-                                </span>
-                            </div>
-
-                            <div className="condition-symbol">
-                                <span className="symbol-and">&</span>
-                                <span className="symbol-or">or</span>
-                            </div>
-                        </div>
+                        {segmentConditions.map((segmentCondition, idx) => {
+                            return (
+                                <SingleCondition
+                                key={idx}
+                                segmentCondition={segmentCondition}
+                                />
+                            );
+                        })}
 
                     </div>
 
