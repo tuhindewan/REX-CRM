b3fa553af2cec940353744c1fbbedfbad2e798cf
abbelal <belal@coderex.co>
2022-10-26 09:36:32 +0600

    segment header and title design done

--- src/components/Segment/CreateSegment.jsx
+++ src/components/Segment/CreateSegment.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import DoubleAngleLeftIcon from '../Icons/DoubleAngleLeftIcon'
 
 const CreateSegment = () => {
-  return (
-    <div>CreateSegments</div>
-  )
+    return (
+        <>
+            <div className="add-segment-page">
+                <button className="backto-segment">
+                    <DoubleAngleLeftIcon />
+                    Back
+                </button>
+
+                <h4 className="add-segment-title">Add Segment</h4>
+
+                <div className="segment-wrapper">
+                    <div className="segment-header">
+                        <div className="form-group">
+                            <label htmlFor="">Segment Name</label>
+                            <input type="text" name="segment-name" />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="">Segment Description</label>
+                            <input type="text" name="segment-description" />
+                        </div>
+
+                        <div className="form-group submit-btn">
+                            <label htmlFor="">submit</label>
+                            <button className="mintmrm-btn">Save <span className="mintmrm-loader"></span> </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default CreateSegment
