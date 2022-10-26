ae5a60f7e34f402845ec454d955b3b7f2b195afb
ashrafun-nesa <ashrafunnesa.soron@gmail.com>
2022-10-26 11:44:40 +0600

    Pull feature/segment-page-design branch

--- src/components/Segment/SegmentList.jsx
+++ src/components/Segment/SegmentList.jsx
@@ -6,73 +6,89 @@ import EditIcon from "../Icons/EditIcon";
 import Delete from "../Icons/Delete";
 import ListenForOutsideClicks from "../ListenForOutsideClicks";
 
-export default function SegmentList() {
-  const [isActiveDropdown, setIsActiveDropdown] = useState(false);
-  const [listening, setListening] = useState(false);
+export default function SegmentList(props) {
+    const {
+        key,
+        segment,
+        selected,
+        deleteSegment,
+        currentActive,
+        setCurrentActive,
+        handleSelectOne,
+    } = props;
+    const { title, data, created_at, total_contacts, id } = segment;
+    const [isActiveDropdown, setIsActiveDropdown] = useState(false);
+    const [listening, setListening] = useState(false);
 
-  const menuButtonRef = useRef(null);
-  const moreOptionRef = useRef(null);
+    const menuButtonRef = useRef(null);
+    const moreOptionRef = useRef(null);
 
-  useEffect(
-    ListenForOutsideClicks(
-      listening,
-      setListening,
-      moreOptionRef,
-      setIsActiveDropdown
-    )
-  );
+    useEffect(
+        ListenForOutsideClicks(
+            listening,
+            setListening,
+            moreOptionRef,
+            setIsActiveDropdown
+        )
+    );
 
-  return (
-    <tr>
-      <td>
-        <span class="mintmrm-checkbox" title="">
-          <input type="checkbox" id="single-select" name="single-select" />
-          <label for="single-select">Vip</label>
-        </span>
-      </td>
-      <td className="">100</td>
-      <td className="">
-        {/* {data?.length > 20 ? data.substring(0, 20) + "..." : data} */}
-        hello how are you
-      </td>
-      <td className="">
-        {/* {new Date(created_at).toDateString()} */}
-        Tue Oct 25 2022
-      </td>
-      <td ref={moreOptionRef}>
-        <button
-          className="more-option"
-          onClick={() => {
-            setIsActiveDropdown((prev) => !prev);
-          }}
-          ref={menuButtonRef} // we need to add ref to menu button in order to correctly position the hovermenu
-        >
-          <ThreeDotIcon />
-          {isActiveDropdown && ( // only show the menu if both active and current active points to this listitem
-            <Portal>
-              <HoverMenu elementRef={menuButtonRef} x={-10} y={-20}>
-                <ul
-                  style={{ width: "190px" }}
-                  className={
-                    isActiveDropdown // only show the menu  current active points to this listitem id
-                      ? "mintmrm-dropdown show"
-                      : "mintmrm-dropdown"
-                  }
+    return (
+        <tr key={key}>
+            <td>
+                <span class="mintmrm-checkbox" title="">
+                    <input
+                        type="checkbox"
+                        name={id}
+                        id={id}
+                        onChange={handleSelectOne}
+                        checked={selected.includes(id)}
+                    />
+                    <label for={id}>{title}</label>
+                </span>
+            </td>
+            <td className="">{total_contacts}</td>
+            <td className="">
+                {data?.length > 20 ? data.substring(0, 20) + "..." : data}
+            </td>
+            <td className="">{new Date(created_at).toDateString()}</td>
+            <td ref={moreOptionRef}>
+                <button
+                    className="more-option"
+                    onClick={() => {
+                        setIsActiveDropdown((prev) => !prev);
+                    }}
+                    ref={menuButtonRef} // we need to add ref to menu button in order to correctly position the hovermenu
                 >
-                  <li>
-                    <EditIcon />
-                    Edit
-                  </li>
-                  <li>
-                    <Delete />
-                    Delete
-                  </li>
-                </ul>
-              </HoverMenu>
-            </Portal>
-          )}
-        </button>
-      </td>
-    </tr>
-  );
+                    <ThreeDotIcon />
+                    {isActiveDropdown && ( // only show the menu if both active and current active points to this listitem
+                        <Portal>
+                            <HoverMenu
+                                elementRef={menuButtonRef}
+                                x={-10}
+                                y={-20}
+                            >
+                                <ul
+                                    style={{ width: "190px" }}
+                                    className={
+                                        isActiveDropdown // only show the menu  current active points to this listitem id
+                                            ? "mintmrm-dropdown show"
+                                            : "mintmrm-dropdown"
+                                    }
+                                >
+                                    <li>
+                                        <EditIcon />
+                                        Edit
+                                    </li>
+                                    <li onClick={() => deleteSegment(id)}>
+                                        <Delete />
+                                        Delete
+                                    </li>
+                                </ul>
+                            </HoverMenu>
+                        </Portal>
+                    )}
+                </button>
+            </td>
+        </tr>
+    );
 }
