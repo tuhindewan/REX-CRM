cc13e8d302b14903e602e0942fa5d50abd600bf7
Tuhin <tohin@coderex.co>
2022-11-02 09:56:59 +0600

    Remove git conflict before deploy

--- app/Internal/FormBuilder/build/index.asset.php
+++ app/Internal/FormBuilder/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-format-library', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-media-utils'), 'version' => 'e1e1e31f6b2fc4ed8bd2');
-=======
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-format-library', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-media-utils'), 'version' => '92b50bee44fc3543f270');
->>>>>>> origin/feat/change-campaign-status
