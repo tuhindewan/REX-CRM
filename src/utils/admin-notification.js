1bc3b62bc791047c9c6fb97b0b1e8e09bf08bc67
Md hr shahin <shahin@coderex.co>
2022-11-03 16:34:51 +0600

    Add comment

--- src/utils/admin-notification.js
+++ src/utils/admin-notification.js
@@ -1,4 +1,9 @@
-
+/**
+ * Add Notification for Success
+ * @param Notification
+ * @returns {function(): void}
+ * @constructor
+ */
 export function AddSuccessNotification(Notification){
     Notification.event('block');
     const timer = setTimeout(() => {
@@ -6,12 +11,30 @@ export function AddSuccessNotification(Notification){
     }, 3000);
     return () => clearTimeout(timer);
 }
+
+/**
+ * Clear Notification
+ * @param value
+ * @param event
+ * @returns {function(): void}
+ * @constructor
+ */
 export function ClearNotification(value,event){
     const timer = setTimeout(() => {
         event(value);
     }, 3000);
     return () => clearTimeout(timer);
 }
+
+/**
+ * Clear Success Notification
+ * Clear Warning Notification
+ * @param value
+ * @param event
+ * @param warring_event
+ * @returns {function(): void}
+ * @constructor
+ */
 export function ClearNotificationWithWarring(value,event,warring_event){
     const timer = setTimeout(() => {
         event(value);
@@ -20,6 +43,13 @@ export function ClearNotificationWithWarring(value,event,warring_event){
     return () => clearTimeout(timer);
 }
 
+/**
+ * Clear Only warning Notification
+ * @param value
+ * @param event
+ * @returns {function(): void}
+ * @constructor
+ */
 export function ClearWarning(value,event){
     const timer = setTimeout(() => {
         event(value);
