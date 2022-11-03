4340892b94f25a16955a118af7415d59c38afea7
monir-coderex <monir@coderex.co>
2022-11-03 15:08:24 +0600

    feat: add dynamic placeholder support - functionalities tested and ok

--- app/Internal/Cron/CampaignsBackgroundProcess.php
+++ app/Internal/Cron/CampaignsBackgroundProcess.php
@@ -183,9 +183,10 @@ class CampaignsBackgroundProcess
                     $email_builder 	= CampaignEmailBuilderModel::get( $scheduled_email_id );
 					$email 			= CampaignModel::get_campaign_email_by_id( $campaign_id, $scheduled_email_id);
                     $email_body 	= isset( $email_builder[ 'email_body' ] ) 	? $email_builder[ 'email_body' ] : '';
-                    $sender_email 	= $email->sender_email;
-                    $sender_name 	= $email->sender_name;
-                    $email_subject 	= $email->email_subject;
+                    $sender_email 	= isset( $email->sender_email ) ? $email->sender_email : '';
+                    $sender_name 	= isset( $email->sender_name ) ? $email->sender_name : '';
+                    $email_subject 	= isset( $email->email_subject ) ? self::update_dynamic_placeholders( $email->email_subject, $contact_id ) : '';
+                    $email_preview 	= isset( $email->email_preview_text ) ? self::update_dynamic_placeholders( $email->email_preview_text, $contact_id ) : '';
 
                     $from = 'From: '. $sender_name;
                     $headers[] = $from . ' <' . $sender_email . '>';
@@ -397,11 +398,11 @@ class CampaignsBackgroundProcess
      * @return array|string|string[]
      * @since 1.0.0
      */
-    private function update_subject_placeholders( string $email_subject, int $contact_id ) {
-        $email_subject = str_replace( '{{first_name}}', ContactData::get_info( $contact_id, 'first_name' ), $email_subject );
-        $email_subject = str_replace( '{{last_name}}', ContactData::get_info( $contact_id, 'last_name' ), $email_subject );
-        $email_subject = str_replace( '{{email}}', ContactData::get_info( $contact_id, 'email' ), $email_subject );
-        $email_subject = str_replace( '{{city}}', ContactData::get_meta( $contact_id, 'city' ), $email_subject );
-        return $email_subject;
+    private function update_dynamic_placeholders( string $data, int $contact_id ) {
+        $data = str_replace( '{{first_name}}', ContactData::get_info( $contact_id, 'first_name' ), $data );
+        $data = str_replace( '{{last_name}}', ContactData::get_info( $contact_id, 'last_name' ), $data );
+        $data = str_replace( '{{email}}', ContactData::get_info( $contact_id, 'email' ), $data );
+        $data = str_replace( '{{city}}', ContactData::get_meta( $contact_id, 'city' ), $data );
+        return $data;
     }
 }
