7f7eb79bb469ab42c10d64bbba39b5497e745852
ashrafun-nesa <ashrafunnesa.soron@gmail.com>
2022-11-15 15:19:07 +0600

    Fix double optin settings

--- src/pages/Settings/DoubleOptin.jsx
+++ src/pages/Settings/DoubleOptin.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import SettingsNav from "./SettingsNav";
 import EmailPendingIcon from "../../components/Icons/EmailPendingIcon";
 import TooltipQuestionIcon from "../../components/Icons/TooltipQuestionIcon";
 
 export default function DoubleOptin() {
+    const [selectOption, setSelectOption] = useState("message");
+    const onChangeValue = (e) => {
+        setSelectOption(e.target.value);
+    };
     return (
         <>
             <div className="mintmrm-settings-page">
@@ -64,7 +68,7 @@ export default function DoubleOptin() {
                                                     placeholder="Enter Email Subject"
                                                 />
                                             </div>
-                                            <div className="form-group">
+                                            <div className="form-group top-align">
                                                 <label htmlFor="">
                                                     Email Body
                                                     <span class="mintmrm-tooltip">
@@ -94,46 +98,77 @@ export default function DoubleOptin() {
                                                         </p>
                                                     </span>
                                                 </label>
-                                                <span className="mintmrm-radiobtn">
-                                                    <input
-                                                        id="show-message"
-                                                        type="radio"
-                                                        name="message-redirect"
-                                                        checked
-                                                    />
-                                                    <label for="show-message">
-                                                        Show Message
-                                                    </label>
-                                                </span>
-                                                <span className="mintmrm-radiobtn">
-                                                    <input
-                                                        id="redirect-url"
-                                                        type="radio"
-                                                        name="message-redirect"
-                                                    />
-                                                    <label for="redirect-url">
-                                                        Redirect to an URL
-                                                    </label>
-                                                </span>
-                                            </div>
-                                            <div className="form-group">
-                                                <label htmlFor="">
-                                                    Email Subject
-                                                    <span class="mintmrm-tooltip">
-                                                        <TooltipQuestionIcon />
-                                                        <p>
-                                                            Define behaviour of
-                                                            the form after
-                                                            submission
-                                                        </p>
+                                                <div onChange={onChangeValue}>
+                                                    <span className="mintmrm-radiobtn">
+                                                        <input
+                                                            id="show-message"
+                                                            type="radio"
+                                                            name="message-redirect"
+                                                            value="message"
+                                                            checked={
+                                                                selectOption ===
+                                                                "message"
+                                                            }
+                                                        />
+                                                        <label for="show-message">
+                                                            Show Message
+                                                        </label>
                                                     </span>
-                                                </label>
-                                                <input
-                                                    type="text"
-                                                    name="email-subject"
-                                                    placeholder="Enter Email Subject"
-                                                />
+                                                    <span className="mintmrm-radiobtn">
+                                                        <input
+                                                            id="redirect-url"
+                                                            type="radio"
+                                                            name="message-redirect"
+                                                            value="redirect"
+                                                            checked={
+                                                                selectOption ===
+                                                                "redirect"
+                                                            }
+                                                        />
+                                                        <label for="redirect-url">
+                                                            Redirect to an URL
+                                                        </label>
+                                                    </span>
+                                                </div>
                                             </div>
+                                            {selectOption === "message" ? (
+                                                <div className="form-group top-align">
+                                                    <label htmlFor="">
+                                                        Confirmation Message
+                                                        <span class="mintmrm-tooltip">
+                                                            <TooltipQuestionIcon />
+                                                            <p>
+                                                                Define behaviour
+                                                                of the form
+                                                                after submission
+                                                            </p>
+                                                        </span>
+                                                    </label>
+                                                    <textarea
+                                                        rows="3"
+                                                        placeholder="Enter Confirmation Message"
+                                                    ></textarea>
+                                                </div>
+                                            ) : (
+                                                <div className="form-group">
+                                                    <label htmlFor="">
+                                                        Redirect URL
+                                                        <span class="mintmrm-tooltip">
+                                                            <TooltipQuestionIcon />
+                                                            <p>
+                                                                Define behaviour
+                                                                of the form
+                                                                after submission
+                                                            </p>
+                                                        </span>
+                                                    </label>
+                                                    <input
+                                                        type="url"
+                                                        name="redirect"
+                                                        placeholder="Enter Redirect URL"
+                                                    />
+                                                </div>
+                                            )}
                                         </div>
                                     </div>
 
