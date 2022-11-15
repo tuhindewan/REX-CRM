fa4301c1789be79dca05c71a1e8b38ff0c328f7a
Md hr shahin <shahin@coderex.co>
2022-11-15 17:16:19 +0600

    Create POST api

--- app/Admin/API/Controllers/BusinessSettingController.php
+++ app/Admin/API/Controllers/BusinessSettingController.php
@@ -22,14 +22,40 @@ Class BusinessSettingController extends SettingBaseController{
 
 	/**
 	 * Get and send response to create a new settings
+	 * @param WP_REST_Request $request
 	 *
-	 * @param WP_REST_Request
-	 * @return WP_REST_Response
-	 * @since 1.0.0
+	 * @return array|WP_REST_Response|\WP_Error
 	 */
 	public function create_or_update( WP_REST_Request $request ){
+
 		$params = MRM_Common::get_api_params_values( $request );
-		error_log(print_r($params,1));
+		if(is_array($params)){
+			if(!empty($params)){
+
+				$business_name  = isset($params['business_name']) ? sanitize_text_field($params['business_name']) : '';
+				$phone          = isset($params['phone']) ? sanitize_text_field($params['phone']) : '';
+				$address        = isset($params['address']) ? sanitize_text_field($params['address']) : '';
+				$logo_url       = isset($params['logo_url']) ? sanitize_text_field($params['logo_url']) : '';
+				$social         = isset($params['social']) ? sanitize_text_field($params['social']) : [];
+				if(!$this->phone_number_validation($phone))	{
+					return $this->get_error_response(__( 'Phone number format is not correct', 'mrm' ));
+				}
+				$business_options = array(
+					"business_name" => $business_name,
+					"phone"         => $phone,
+					"address"       => $address,
+					"logo_url"      => $logo_url,
+					"social"        => $social
+				);
+
+				update_option('_mrm_business_info_setting',$business_options);
+				return $this->get_success_response(__( 'Business information settings has been successfully saved.', 'mrm' ));
+			}
+		}
+
+		return $this->get_error_response(__( 'Failed to get data', 'mrm' ), 400);
+
+
 
 	}
 
@@ -45,6 +71,11 @@ Class BusinessSettingController extends SettingBaseController{
 
 		error_log(print_r($key,1));
 
+	}
+
+	public function phone_number_validation( $number ) {
+		$phone_number_validation_regex = "/^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$/";
+		return preg_match($phone_number_validation_regex, $number);
 	}
 
 }
---
+++ tests/settings/test-business-info-setting-controller.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Class Test_Setting_Controller
+ *
+ * @package mrm
+ */
+
+/**
+ * Test cases for the functions of the production
+ * class Rex_Product_Data_Retriever.
+ *
+ * @see /app/Admin/API/Controllers/SettingController.php
+ */
+
+class Test_BusinessSettingController extends WP_UnitTestCase {
+	private static $instance;
+	private static $reflector;
+
+	/**
+	 * @throws ReflectionException
+	 */
+	public function setUp():void
+	{
+		self::$instance      = \Mint\MRM\Admin\API\Controllers\BusinessSettingController::get_instance();
+	}
+
+
+	public function test_create_or_update() {
+		$request = new WP_REST_Request( 'POST', '/mrm/v1/settings/business' );
+		$business_options = array(
+			"business_name" => "",
+			"phone"         => "+8800178294804",
+			"address"       => "string",
+			"logo_url"      => "String",
+			"social"        => [
+				"icon"      => "String",
+				"url"       => "String"
+			]
+		);
+		$request->set_body( json_encode( $business_options ) );
+		$response = self::$instance->create_or_update( $request );
+		$this->assertTrue(is_object($response) && get_class($response) ===  'WP_REST_Response' || get_class($response) ===  'WP_Error');
+
+		$business_options_data = '';
+		$expected = is_array($business_options_data);
+		$actual = '';
+		$this->assertEquals($actual , $expected);
+
+
+		$expected = self::$instance->phone_number_validation( $business_options['phone'] );
+		$actual = true;
+		$this->assertTrue($actual , $expected);
+
+
+	}
+
+	public function test_phone_number_validation(  ) {
+
+		$excepted = self::$instance->phone_number_validation( 'aklhdkjfahdskjf' );
+		$actual = false;
+		$this->assertEquals( $excepted, $actual);
+
+
+		$excepted = self::$instance->phone_number_validation( '(541) 754-3010' );
+		$actual = false;
+		$this->assertEquals( $excepted, $actual);
+
+		$excepted = self::$instance->phone_number_validation( '541 754-3010' );
+		$actual = true;
+		$this->assertEquals( $excepted, $actual);
+
+		$excepted = self::$instance->phone_number_validation( '+8800178294804' );
+		$actual = true;
+		$this->assertEquals( $excepted, $actual);
+
+	}
+
+
+
+}
