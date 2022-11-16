d3cf29faf0e4d577ec9337da68f84eda9c853cb9
abbelal <belal@coderex.co>
2022-11-16 14:07:19 +0600

    custom field modal design done

--- src/pages/Settings/CustomFieldSettings.jsx
+++ src/pages/Settings/CustomFieldSettings.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 
 import SettingsNav from "./SettingsNav";
 import SingleCustomField from "./CustomField/SingleCustomField";
+import AddCustomFieldModal from "./CustomField/AddCustomFieldModal";
 
 import CustomFieldIcon from "../../components/Icons/CustomFieldIcon";
 import PlusIcon from "../../components/Icons/Plus";
@@ -9,6 +10,15 @@ import NoCustomFieldIcon from "../../components/Icons/NoCustomFieldIcon";
 
 
 export default function CustomFieldSettings() {
+    const [customFieldModal, setCustomFieldModal] = useState(false);
+
+    const addCustomField = () => {
+        setCustomFieldModal(!customFieldModal);
+    };
+
+    const closeCustomFieldModal = () => {
+        setCustomFieldModal(false);
+    };
 
     return (
         <>
@@ -22,6 +32,13 @@ export default function CustomFieldSettings() {
 
                             <div className="settings-tab-content">
                                 <div className="single-tab-content custom-field-tab-content">
+
+                                    <div className={customFieldModal ? 'add-custom-field-modal show-modal' : 'add-custom-field-modal'}>
+                                        <AddCustomFieldModal 
+                                            closeCustomFieldModal= {closeCustomFieldModal}
+                                        />
+                                    </div>
+
                                     <div className="tab-body">
                                         <header className="tab-header">
                                             <h4 className="title">
@@ -33,7 +50,7 @@ export default function CustomFieldSettings() {
                                         <div className="form-wrapper">
                                             <div className="form-header">
                                                 <h4>Add Custom Field</h4>
-                                                <button type="button" className="mintmrm-btn" title="Add Custom Field" >
+                                                <button type="button" className="mintmrm-btn" title="Add Custom Field" onClick={addCustomField} >
                                                     <PlusIcon/>
                                                     Add Field
                                                 </button>
