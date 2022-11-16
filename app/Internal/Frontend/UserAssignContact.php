d70b4dafd644276804762f5827d0c277c2771d42
Md hr shahin <shahin@coderex.co>
2022-11-16 16:13:53 +0600

    Add comment user in COntact

--- app/Internal/Frontend/UserAssignContact.php
+++ app/Internal/Frontend/UserAssignContact.php
@@ -36,6 +36,7 @@ class UserAssignContact {
 	public function __construct()
 	{
 		add_action('user_register', array($this, 'assign_signup_user_in_contact'),10,2 );
+		add_action('comment_post', array($this, 'assign_comment_post_user_in_contact'),10,3 );
 	}
 
 	/**
@@ -48,6 +49,7 @@ class UserAssignContact {
 	public function assign_signup_user_in_contact ( $user_id , $user_data) {
 
 		$get_options = get_option('_mrm_general_user_signup');
+
 		if (isset($get_options['enable']) && $get_options['enable']){
 			if(isset($get_options['list_mapping']) && is_array($get_options['list_mapping'])){
 				foreach ($get_options['list_mapping'] as $lists){
@@ -70,6 +72,45 @@ class UserAssignContact {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Assign Commer User to Mint Email
+	 * @param $comment_ID
+	 * @param $comment_approved
+	 * @param $commentdata
+	 *
+	 * @return void
+	 */
+	public function assign_comment_post_user_in_contact(  $comment_ID, $comment_approved, $commentdata ) {
+
+		$get_options = get_option('_mrm_general_comment_form_subscription');
+		if (isset($get_options['enable']) && $get_options['enable']){
+			$user_email = isset($commentdata['comment_author_email']) ? $commentdata['comment_author_email'] : '';
+			if ('' != $user_email ){
+				$exist_email    = ContactModel::is_contact_exist( $user_email );
+				if(!$exist_email) {
+					$contact        = new ContactData( $user_email,$parms = [] );
+					$contact_id     = ContactModel::insert( $contact );
+					if ($contact_id){
+						$get_double_optin = get_option('_mrm_optin_settings');
+						$is_double_optin = isset($get_double_optin['optin']['enable']) ? $get_double_optin['optin']['enable'] : false;
+						if($is_double_optin){
+							MessageController::get_instance()->send_double_opt_in( $contact_id );
+						}
+						$group_tag = isset($get_options['tags']) ? $get_options['tags'] : [];
+						$group_list = isset($get_options['lists']) ? $get_options['lists'] : [];
+						$group_data = array_merge($group_tag,$group_list);
+						$ids = [];
+						foreach ($group_data as $id){
+							$ids[] = $id;
+						}
+						TagController::set_tags_to_contact($ids,$contact_id);
+					}
+				}
+			}
+		}
+
 	}
 
 	/**
