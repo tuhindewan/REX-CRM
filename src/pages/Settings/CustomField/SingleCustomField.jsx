d9a60d83a29c38724eb3e4f52eed359d6d6f95fb
abbelal <belal@coderex.co>
2022-11-16 15:45:10 +0600

    custom field delete confirmation modal added

--- src/pages/Settings/CustomField/SingleCustomField.jsx
+++ src/pages/Settings/CustomField/SingleCustomField.jsx
@@ -3,17 +3,57 @@ import React, { useRef, useState } from "react";
 import EditIcon from "../../../components/Icons/EditIcon";
 import DeleteIcon from "../../../components/Icons/Delete";
 import ThreeDotIcon from "../../../components/Icons/ThreeDotIcon";
+import CrossIcon from "../../../components/Icons/CrossIcon";
 
 
 export default function SingleCustomField({index, deleteCustomField}) {
+
     const [showModal, setShowModal] = useState(false);
+    const [confirmationModal, setConfirmationModal] = useState(false);
 
     const showMoreOption = (index) => {
         setShowModal(!showModal);
     }
+    
+    //----delete confirmation modal-----
+    const deleteConfirmationAlert = (index) => {
+        setConfirmationModal(!confirmationModal);
+    };
+    
+    //----delete confirmation modal-----
+    const cancelConfirmationModal = (index) => {
+        setConfirmationModal(false);
+    };
 
     return (
         <>
+            {confirmationModal &&
+                <div className="mintmrm-delete-alert-wrapper" >
+                    <div className="mintmrm-delete-confirmation">
+                        <div className="delete-confirmation-header">
+                            <h3>Delete Custom Field {index}</h3>
+                            <div className="cross-icon" onClick={cancelConfirmationModal}>
+                                <CrossIcon />
+                            </div>
+                        </div>
+
+                        <div className="delete-confirmation-body">
+                            <DeleteIcon />
+                            <p>Are you sure, You wnat to delete it ?</p>
+                        </div>
+
+                        <ul className="mintmrm-delete-confirm-btn">
+                            <li>
+                                <button className="btn-default cancel" onClick={cancelConfirmationModal}> Cancel </button>
+                            </li>
+                            <li>
+                                <button type="button" className="btn-default delete" onClick={() => deleteCustomField(index)}> Delete </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            }
+
             <div className="single-custom-field">
                 <h5 className="field-name">Country</h5>
 
@@ -25,7 +65,7 @@ export default function SingleCustomField({index, deleteCustomField}) {
                             <EditIcon/>
                             Edit
                         </li>
-                        <li onClick={() => deleteCustomField(index)}>
+                        <li onClick={deleteConfirmationAlert}>
                             <DeleteIcon/>
                             Delete
                         </li>
