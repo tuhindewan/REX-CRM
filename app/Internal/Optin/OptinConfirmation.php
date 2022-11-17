4aa16f42bc75a3bc1d8228e52405e7f15218a483
Tuhin <tohin@coderex.co>
2022-11-17 11:58:31 +0600

    MINTCRM-350: Apply double optin settings while sending double optin email            - add conditional confirmation type after subscription

--- app/Internal/Optin/OptinConfirmation.php
+++ app/Internal/Optin/OptinConfirmation.php
@@ -52,6 +52,18 @@ class OptinConfirmation
 		$get = MRM_Common::get_sanitized_get_post();
 		$get = isset($get['get']) ? $get['get'] : [];
 		if( isset( $get['mrm'] ) && isset( $get['route'] ) && $get['route'] == 'confirmation' )  {
+
+			$default    = [
+				"enable"                => true,
+				"email_subject"         => "Please Confirm Subscription.",
+				"email_body"            => "Please Confirm Subscription. {{subscribe_link}}. <br> If you receive this email by mistake, simply delete it.",
+				"confirmation_type"     => "message",
+				"confirmation_message"  => "Subscription Confirmed. Thank you."
+			];
+
+			$settings   = get_option( "_mrm_optin_settings", $default );
+			$confirmation_type = isset( $settings['confirmation_type'] ) ? $settings['confirmation_type'] : "";
+			error_log(print_r($settings, 1));
 			$contact_id = isset( $get['contact_id'] ) ? $get['contact_id'] : "";
 			$hash 		= isset( $get['hash'] ) ? $get['hash'] : "";
 
@@ -81,7 +93,16 @@ class OptinConfirmation
 				NoteModel::insert( $note, $contact_id );
 
 			}
-            require_once(MRM_DIR_PATH. 'app/Resources/public/confirmation.php');
+			if( "message" == $confirmation_type ){
+				$confirmation_message = isset( $settings['confirmation_message'] ) ? $settings['confirmation_message'] : "";
+				require_once(MRM_DIR_PATH. 'app/Resources/public/confirmation.php');
+			}else if( "redirect" == $confirmation_type ){
+				$url = isset( $settings['url'] ) ? $settings['url'] : "";
+				wp_redirect( $url );
+			}else{
+				$page_id = isset( $settings['page_id'] ) ? $settings['page_id'] : "";
+				wp_redirect( get_permalink( $page_id ) );
+			}
 			die();
 		}
 		
