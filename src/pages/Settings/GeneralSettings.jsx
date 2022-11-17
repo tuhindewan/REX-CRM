e5863494d577e9e8ce8c2e49126a4ebfc5805623
Md hr shahin <shahin@coderex.co>
2022-11-17 16:17:46 +0600

    Fix user assign issue

--- src/pages/Settings/GeneralSettings.jsx
+++ src/pages/Settings/GeneralSettings.jsx
@@ -129,7 +129,22 @@ export default function GeneralSettings() {
                 setAssignTags(comment_form_subscription.tags);
             }
             if(Object.keys(user_signup).length > 0){
-                setUserSelectSwitch(user_signup.enable)
+                setUserSelectSwitch(user_signup.enable);
+                if(user_signup.list_mapping.length  > 0){
+                    user_signup.list_mapping.map(function (value, index){
+                       if (value.role == 'administrator'){
+                            setAssignAdministratorLists(value.list)
+                       }if (value.role == 'editor'){
+                           setAssignEditorLists(value.list)
+                       }if (value.role == 'author'){
+                           setAssignAuthorLists(value.list)
+                       }if (value.role == 'contributor'){
+                           setAssignContributorLists(value.list)
+                       }if (value.role == 'subscriber'){
+                           setAssignSubscriberLists(value.list)
+                       }
+                    })
+                }
             }
         });
     }, []);
@@ -164,19 +179,19 @@ export default function GeneralSettings() {
                 list_mapping : [
                     {
                         role : 'administrator',
-                        list : ''
+                        list : assignAdministratorLists
                     }, {
                         role : 'editor',
-                        list : ''
+                        list : assignEditorLists
                     }, {
                         role : 'author',
-                        list : ''
+                        list : assignAuthorLists
                     }, {
                         role : 'contributor',
-                        list : ''
+                        list : assignContributorLists
                     },{
                         role : 'subscriber',
-                        list : ''
+                        list : assignSubscriberLists
                     }
                 ]
 
